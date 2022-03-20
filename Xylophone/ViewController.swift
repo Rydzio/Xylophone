a48718e9266342abc9bf101a84985e122c29752e
Michal Rytych <michal.rytych@geminibs.com>
2022-03-20 13:51:05 +0100

    Final project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        makeOpaque(backgroundColor: sender)
+        
+    }
     
-
+    func playSound(soundName : String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func makeOpaque (backgroundColor : UIButton) {
+        backgroundColor.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            backgroundColor.alpha = 1.0
+        }
+    }
+    
 }
 
